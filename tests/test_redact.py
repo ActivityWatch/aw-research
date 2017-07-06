7d877aeaa66d72db2eaf1f21df91040047ac2d87
Erik Bjäreholt <erik@bjareho.lt>
2017-07-07 00:28:21 +0200

    improved test

--- tests/test_redact.py
+++ tests/test_redact.py
@@ -4,7 +4,8 @@ from aw_analysis.redact import redact_words
 
 
 def test_redact_word():
-    e = Event(data={"label": "sensitive"})
+    e = Event(data={"label": "sensitive", "desc": "Lorem ipsum..."})
     e = redact_words([e], ["sensitive"])[0]
     assert "sensitive" not in e.data["label"]
-
+    assert "redacted" in e.data["label"]
+    assert "redacted" in e.data["desc"]
